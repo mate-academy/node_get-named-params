b8d29659f5a9c23b532a9db466d94c72ee7120ae
Dmytro Tsehelnyk <dtsehelnyk@cybervisiontech.com>
2021-11-07 23:57:25 +0200

    solution

---
+++ src/__test__/test.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const getSentence = require('../app');
+
+test('', () => expect(getSentence()).toBe("can't push this without tests"));
--- src/app.js
+++ src/app.js
@@ -2,7 +2,13 @@
 
 const minimist = require('minimist');
 
-const { name, lastname, age } = minimist(process.argv);
+const getSentence = () => {
+  const { name, lastname, age } = minimist(process.argv);
 
-// eslint-disable-next-line no-console
-console.log(`${name} ${lastname} is ${age}`);
+  // eslint-disable-next-line no-console
+  console.log(`${name} ${lastname} is ${age}`);
+
+  return "can't push this without tests";
+};
+
+module.exports = getSentence;
