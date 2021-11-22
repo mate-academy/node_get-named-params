73d48ce247d5144627054009bb802a6c42b1fd60
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-11-22 19:32:25 +0200

    Named params solution added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const LASTNAME = 'lastname';
+const NAME = 'name';
+const AGE = 'age';
+
+const params = Object.values(process.argv.slice(2)).flat();
+
+let lastname;
+let name;
+let age;
+
+for (const param of params) {
+  const indexOfEquals = param.indexOf('=');
+  const key = param.slice(2, indexOfEquals);
+  const value = param.slice(indexOfEquals + 1);
+
+  switch(key) {
+    case LASTNAME:
+      lastname = value;
+      break;
+
+    case NAME:
+      name = value;
+      break;
+
+    case AGE:
+      age = value;
+      break;
+
+    default:
+      throw new Error('Please check the input data');
+  }
+}
+
+return `${name} ${lastname} is ${age}`;
