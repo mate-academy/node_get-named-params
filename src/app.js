fb0907d765b455dfcd8810f124b62ef07a9c7163
Valerie Yaskevych <redrum.back@gmail.com>
2021-12-19 03:07:00 +0200

    solved

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const minimist = require('minimist');
+
+const introduce = ({ age, name, lastname }) => (
+  `${name} ${lastname} is ${age}`
+);
+
+console.log(
+  introduce(minimist(process.argv))
+);
