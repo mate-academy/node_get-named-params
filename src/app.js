1a0f53ffa575c7177e7bb560953ebf672721ca03
Andriy Bulbuk <bulbuk.andriy@gmail.com>
2022-01-20 13:29:42 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const minimist = require('minimist');
+
+function printMessage(enteredData) {
+  const { name, lastname, age } = minimist(enteredData);
+
+  // eslint-disable-next-line no-console
+  console.log(`${name} ${lastname} is ${age}`);
+};
+
+printMessage(process.argv);
