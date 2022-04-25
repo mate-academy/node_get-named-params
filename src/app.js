a4f1c9f208bc0de94845737f840f46739780e93f
Vsevolod Maslak <vsevolodmaslak@gmail.com>
2022-04-25 13:28:31 +0300

    final

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+/* eslint-disable no-console */
 'use strict';
+
+const minimist = require('minimist');
+
+const person = minimist(process.argv.slice(2));
+
+console.log(`${person.name} ${person.lastname} is ${person.age}`);
