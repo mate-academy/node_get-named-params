d72040c37d1d281dcfa6091b5ad99d0e35689525
unknown <xpromt@gmail.com>
2022-05-04 19:13:14 +0300

    Result 00

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-'use strict';
+import readline from 'readline';
+import minimist from 'minimist';
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const { name, lastname, age } = minimist(process.argv.slice(2));
+
+terminal.write(`${name} ${lastname} is ${age} \n`);
+terminal.close();
