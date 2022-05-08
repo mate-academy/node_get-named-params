5d75045c88ba69857d0203e72fab891960cf04a5
vladkoval <kickgames675@gmail.com>
2022-05-08 12:42:25 +0300

    Solution fixes

--- src/app.js
+++ src/app.js
@@ -4,4 +4,5 @@ const minimist = require('minimist');
 
 const { lastname, age, name } = minimist(process.argv.slice(2));
 
+// eslint-disable-next-line no-console
 console.log(`${name} ${lastname} is ${age}`);
