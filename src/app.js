ac0a44fbc95f732d7446d171178f659e64d308bd
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2022-06-02 17:27:49 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
+/* eslint-disable no-console */
 'use strict';
+import minimist from 'minimist';
+
+const inputParams = minimist(process.argv.slice(2));
+
+console.log(
+  `${inputParams.name} ${inputParams.lastname} is ${inputParams.age}`
+);
