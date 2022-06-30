968d6e73908111cf0b2c1e1d1a4cff52036dc746
Nazar Ostafiichuk <90191817+Ostafii4uk@users.noreply.github.com>
2022-06-30 19:35:52 +0300

    Fixed solution

--- src/app.js
+++ src/app.js
@@ -2,10 +2,6 @@
 
 const minimist = require('../node_modules/minimist/index');
 
-const dotenv = require('../node_modules/dotenv/lib/main');
-
-dotenv.config();
-
 const { lastname, name, age } = minimist(process.argv.slice(2));
 
 // eslint-disable-next-line no-console
