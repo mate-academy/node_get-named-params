ae7f9056da32daff4899d0791f49c8bb73de6a24
Nazar Ostafiichuk <90191817+Ostafii4uk@users.noreply.github.com>
2022-06-30 19:09:11 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 'use strict';
+
+import minimist from 'minimist';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { lastname, name, age } = minimist(process.argv.slice(2));
+
+console.log(`${name} ${lastname} is ${age}`);
