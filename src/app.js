a85f03181d3c75df51ffffe3a48953f68d45e155
Alisa Ivashkevych <a.ivashkevych@mate.academy>
2022-07-05 13:29:34 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const minimist = require('minimist');
+
+const { name, lastname, age } = minimist(process.argv.slice(2));
+
+// eslint-disable-next-line
+console.log(
+  `${name} ${lastname} is ${age}`
+);
