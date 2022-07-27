c4ec2b0274e56dcbc55d7d0777f44d6398fc354c
Nikita Karbazin <karbazinnikita@gmail.com>
2022-07-27 14:38:51 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const arrayInput = process.argv.slice(2);
+
+const [lastname, age, name] = arrayInput;
+
+// eslint-disable-next-line no-console
+console.log(`${name} ${lastname} is ${age}`);
