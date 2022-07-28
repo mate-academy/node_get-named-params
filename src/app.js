23625bdab22b7fda209f32d4965b870a882c998c
Ivan Shulhan <92580512+IvanShulhan@users.noreply.github.com>
2022-07-28 11:20:39 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+/* eslint-disable no-console */
 'use strict';
+
+const minimist = require('minimist');
+
+(function showMessage() {
+  const person = minimist(process.argv.slice(2));
+
+  console.log(`${person.name} ${person.lastname} is ${person.age}`);
+})();
