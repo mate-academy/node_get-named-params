069af43119a9ff38e6ec397e6f596ac00b8f1175
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2022-08-12 18:42:17 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const minimist = require('minimist');
+
+const userData = minimist(process.argv.slice(2));
+
+function printUserInfo(userInfo) {
+  return `${userInfo.name} ${userInfo.lastname} is ${userInfo.age}`;
+}
+
+// eslint-disable-next-line no-console
+console.log(printUserInfo(userData));
