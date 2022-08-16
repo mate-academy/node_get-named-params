727fb59798a01c5c594e4d0b25d3585454623c8f
Illia Vodonis <illavodonis@gmail.com>
2022-08-16 23:21:21 +0300

    develop

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+/* eslint-disable no-console */
 'use strict';
+
+const minimist = require('minimist');
+
+const targetInfo = minimist(process.argv.slice(2));
+
+console.log(`${targetInfo.name} ${targetInfo.lastname} is ${targetInfo.age}`);
