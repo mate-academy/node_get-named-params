6c02dbd6620c64502da5347a2ef5eec3dc0e1572
Ilona Shulha <ilon.sh.ilo@gmail.com>
2022-08-17 11:37:57 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const minimist = require('../node_modules/minimist');
+
+function readParams(data) {
+  const { lastname, name, age } = data;
+
+  return `${name} ${lastname} is ${age}`;
+}
+
+const dataFromCommandLine = minimist(process.argv);
+
+// eslint-disable-next-line no-console
+console.log(
+  readParams(dataFromCommandLine)
+);
