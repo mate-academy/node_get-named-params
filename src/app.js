cc97300f07f6b3f259f423f6fc871b6ed0addee3
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-09-06 00:44:44 +0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const minimist = require('minimist');
+
+const handleInput = (input) => {
+  const { lastname, age, name } = minimist(input);
+
+  // eslint-disable-next-line no-console
+  console.log(`${name} ${lastname} is ${age}`);
+};
+
+handleInput(process.argv.slice(2));
