6a521fea3df9f7547645a5c1279657cc61d60ce5
Roman Kovalchuk <roma220702@gmail.com>
2022-09-09 16:05:25 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const minimist = require('minimist');
+
+const func = () => {
+  const obj = minimist(process.argv.slice(2));
+
+  return `${obj.name} ${obj.lastname} is ${obj.age}`;
+};
+
+func();
