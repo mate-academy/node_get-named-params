d12e52d68714ab7a4039cd2e3c973e72c9fc0f08
Aleksandr Yuschenko <105875778+AleksandrYuschenko@users.noreply.github.com>
2022-09-21 15:16:50 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+/* eslint-disable no-console */
 'use strict';
+
+const minimist = require('minimist');
+
+const option = minimist(process.argv.slice(2));
+
+console.log(`${option.name} ${option.lastname} is ${option.age}`);
