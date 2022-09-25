28e0bbae289ca6a973094003b457f7684f9a1f91
sergii2705 <maniurov_sv@ukr.net>
2022-09-25 21:32:15 +0300

    Solution ver 1.0.0

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
 'use strict';
+/* eslint-disable no-console */
+
+const minimist = require('minimist');
+
+const params = minimist(process.argv.slice(2));
+
+console.log(`${params.name} ${params.lastname} is ${params.age}`);
