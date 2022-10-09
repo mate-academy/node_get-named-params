93a11b5131b4e76e755d1aee5b204e16011c333f
Roman Nikitenko <Lomkaaa@ukr.net>
2022-10-09 07:07:07 +0300

    Change connect minimist, but i don't understand why don't work tests

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const minimist = require('minimist');
+
+const user = minimist(process.argv.slice(2));
+
+const result = `${user['name']} ${user['lastname']} ${user['age']}`;
+
+console.log(result);
+
+
