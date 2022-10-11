4090ec6ced76a3b97751e26aa9fe9a3365bf877b
Denys Korotkoruchko <denenergii@gmail.com>
2022-10-11 17:18:00 +0300

    ready

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const minimist = require('minimist');
+
+const person = minimist(process.argv);
+
+// eslint-disable-next-line no-console
+console.log(`${person.name} ${person.lastname} is ${person.age}`);
