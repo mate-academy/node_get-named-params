71c629cb3e2c5300d9cb1e2392b7fe883af60a0d
Synytsyn Denys <yungdena54@gmail.com>
2022-10-12 10:12:38 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+/* eslint-disable no-console */
 'use strict';
+
+const minimist = require('minimist');
+
+const result = minimist(process.argv.slice(2));
+
+console.log(`${result.name} ${result.lastname} is ${result.age}`);
