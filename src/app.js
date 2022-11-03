efc5eba1be55fbb153ad839120502af9e49eee94
ZUBOV ILLIA <zubovillia@gmail.com>
2022-11-03 12:38:16 +0300

    lgtm using minimist

--- src/app.js
+++ src/app.js
@@ -1,30 +1,35 @@
 /* eslint-disable no-console */
 'use strict';
 
-console.log(process.argv.slice(2));
+// const { minimist } = require('minimist');
+const minimist = require('minimist');
+// import minimist from "minimist";
+
+const terminal = minimist(process.argv.slice(2));
+
+console.log(`${terminal.name} ${terminal.lastname} is ${terminal.age}`);
 
 nativeMethod();
 
 function nativeMethod() {
+  // work only in case if variable '--name=10'
   let age;
   let userName;
   let lastname;
 
   for (const item of process.argv.slice(2)) {
     if (item.includes('age')) {
-      age = item.slice(item.indexOf('age=') + 4);
+      age = item.slice(item.indexOf('age') + 4);
     }
 
     if (item.includes('-name')) {
-      userName = item.slice(item.indexOf('name=') + 5);
+      userName = item.slice(item.indexOf('name') + 5);
     }
 
     if (item.includes('lastname')) {
-      lastname = item.slice(item.indexOf('lastname=') + 9);
+      lastname = item.slice(item.indexOf('lastname') + 9);
     }
   }
 
   console.log(`${userName} ${lastname} is ${age}`);
 }
-
-// - `src/app.js --age=25 --name=Jonh --lastname=Galt` prints `John Galt is 25`
