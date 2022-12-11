150dba5a1ed4ab476016c11bee74b12558e1ff5b
Oleksandr Krynytskyi <alex.krynytskyi@gmail.com>
2022-12-11 18:18:18 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+/* eslint-disable no-console */
 'use strict';
+
+const minimist = require('minimist');
+const args = process.argv.splice(2);
+const { name, lastName, age } = minimist(args);
+
+console.log(`${name} ${lastName} is ${age}`);
