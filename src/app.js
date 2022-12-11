482546e2068d04dee2aacd4385190fccf98a49e5
Oleksandr Krynytskyi <alex.krynytskyi@gmail.com>
2022-12-11 18:23:02 +0200

    small fix

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@
 
 const minimist = require('minimist');
 const args = process.argv.splice(2);
-const { name, lastName, age } = minimist(args);
+const { name, lastname, age } = minimist(args);
 
-console.log(`${name} ${lastName} is ${age}`);
+console.log(`${name} ${lastname} is ${age}`);
