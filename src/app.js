692d098f69201173c1d6b81d796b2d14962e7c3d
Lefteris Loizides <loizides.lef@gmail.com>
2023-02-13 18:10:52 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const minimist = require('minimist');
+const info = minimist(process.argv.slice(2));
+const { name, lastname, age } = info;
+
+// eslint-disable-next-line no-console
+console.log(`${name} ${lastname} is ${age}`);
