d10af8532e0a396ed6c997aed00fe0722e1975f0
Demut <uddarendeli1@gmail.com>
2023-02-18 22:39:59 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+function parseArguments() {
+  const result = {};
+
+  for (const arg of process.argv.slice(2)) {
+    if (arg.substring(0, 2) === '--') {
+      const [key, value] = arg.substring(2).split('=');
+
+      result[key] = value;
+    }
+  }
+
+  return result;
+}
+
+const args = parseArguments();
+
+const message = `${args['name']} ${args['lastname']} is ${args['age']}`;
+
+message.toString();
