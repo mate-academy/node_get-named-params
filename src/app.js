a6532b936c3be78ab6ded4ba7eec807367d7e7ed
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2023-02-22 08:34:25 +0100

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+/* eslint-disable no-console */
 'use strict';
+
+const minimist = require('minimist');
+
+const argvs = minimist(process.argv.slice(2));
+
+const message = `${argvs.name} ${argvs.lastname} is ${argvs.age}`;
+
+console.log(message);
