f936e07a07b5914a24042fdbae16f4d306beb8fd
Mohamed Dahi <mohamedjane362@gmail.com>
2023-03-21 08:56:31 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const minimist = require('minimist');
+
+const info = minimist(process.argv);
+
+// eslint-disable-next-line no-console
+console.log(`${info.name} ${info.lastname} is ${info.age}`);
