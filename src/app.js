a68d86d780d10016b2b0fa03ec87f21875cd0f00
Leonid Vegera <leonid.vegera@gmail.com>
2023-03-24 12:42:23 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
 'use strict';
+
+import minimist from 'minimist';
+
+const params = minimist(process.argv.slice(2));
+const user = `${params['name']} ${params['lastname']} is ${params['age']}`
+console.log(user);
