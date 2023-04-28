1210569cfece21ef3db30a9f5d34dc9fe92c69fb
Jānis Žurilo <zurilojohn@gmail.com>
2023-04-28 13:08:05 +0300

    Finished task

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const minimist = require('minimist');
+
+const args = minimist(process.argv.slice(2));
+
+const { name, lastname, age } = args;
+
+// eslint-disable-next-line no-console
+console.log(`${name} ${lastname} is ${age}`);
