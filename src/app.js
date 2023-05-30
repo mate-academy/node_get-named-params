a3fc29b6dc47f1f75639e1d63aaf881dbeaad817
Metin BICAKSIZ <metinbicaksiz@mail.com>
2023-05-30 20:30:10 +0300

    completed

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const args = process.argv.slice(2);
+
+let name = 'John';
+let lastname = 'Doe';
+let age = 30;
+
+args.forEach(arg => {
+  const [paramName, paramValue] = arg.split('=');
+
+  switch (paramName) {
+    case '--name':
+      name = paramValue;
+      break;
+    case '--lastname':
+      lastname = paramValue;
+      break;
+    case '--age':
+      age = paramValue;
+      break;
+    default:
+      break;
+  }
+});
+
+// eslint-disable-next-line no-console
+console.log(`${name} ${lastname} is ${age}`);
